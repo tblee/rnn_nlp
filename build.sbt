099519097c5645b85b9a216c670a38112b8acca6
tblee <ntutblee@gmail.com>
2016-05-12 21:35:22 -0700

    first commmit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "rnn_nlp"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+    
---
+++ src/main/scala/org/apache/spark/shallowNN/shallowNN_Breeze.scala
@@ -0,0 +1,14 @@
+package org.apache.spark.shallowNN
+
+import scala.math
+import scala.util.Random
+import org.apache.spark._
+import org.apache.spark.rdd.RDD
+import breeze.linalg._
+
+/**
+  * Created by tblee on 5/12/16.
+  */
+object shallowNN_Breeze {
+
+}
